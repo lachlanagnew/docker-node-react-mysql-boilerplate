cde84af46b3b7ff7ac40a57cfa02525d9dcbc130
Lachlan Agnew <lachlanagnew@outlook.com>
2019-03-28 14:14:28 +1100

    Added ELK

--- server/index.js
+++ server/index.js
@@ -4,19 +4,132 @@ const cors = require("cors");
 const Sequelize = require("sequelize");
 const dbConfig = require("./config/config.json").development;
 const User = require("./models").User;
+const bodyParser = require('body-parser');
+const elasticsearch = require('elasticsearch');
+const client = new elasticsearch.Client({
+  host: 'elasticsearch:9200'
+});
 
 connectToDatabase();
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 app.use(cors());
-app.get("/", async (req, res) => {
+
+//search
+app.post("/users/search", async (req, res) => {
+  let query = req.body.query;
+  console.log(`Searching users for ${query}`)
   try {
-    const user = await User.findById(1);
-    const response = { message: `This response came from the node.js app. User ${user.username} is on the database.` };
-    res.send(response);
+    const response = await client.search({
+      index: 'example',
+      type: 'users',
+      body: {
+        query: {
+          bool: {
+            should: [
+              {
+                match: {
+                  username: query
+                }
+              },
+              {
+                wildcard: {
+                  username: `*${query}*`
+                }
+              },
+              {
+                fuzzy: {
+                  username: query
+                }
+              }
+            ],
+            minimum_should_match: 1
+          }
+        }
+      }
+    })
+    if (response.hits.total > 0) {
+      const hits = response.hits.hits;
+      res.status(200).send(hits.map((user_data) => {
+        return {
+          username: user_data._source.username,
+          id: user_data._source.id
+        }
+      }))
+    } else {
+        res.status(200).send([]);
+    }
   } catch (error) {
     res.status(422).send(error);
   }
 });
+
+// CREATE
+app.post("/users", async (req, res) => {
+  console.log("Creating User")
+  try {
+    const users = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+    res.status(201).send(users);
+  } catch (error) {
+    res.status(422).send(error);
+  }
+});
+
+// READ ALL
+app.get("/users", async (req, res) => {
+  console.log("Reading All Users")
+  try {
+    const users = await User.all();
+    res.send(users);
+  } catch (error) {
+    res.status(422).send(error);
+  }
+});
+
+// READ
+app.get("/users/:id", async (req, res) => {
+  let id = parseInt(req.params.id)
+  console.log(`Reading User with id ${id}`)
+  try {
+    const user = await User.findById(id);
+    res.send(user);
+  } catch (error) {
+    res.status(422).send(error);
+  }
+});
+
+// UPDATE
+app.put("/users/:id", async (req, res) => {
+  let id = parseInt(req.params.id);
+  console.log(`Update User with id ${id}`)
+  let username = req.body.username;
+  let password = req.body.password;
+  try {
+    let user = await User.update({ username, password }, { where: { id: id } })
+    res.status(202).send(user);
+  } catch (error) {
+    res.status(422).send(error);
+  }
+});
+
+// DELETE
+app.delete("/users/:id", async (req, res) => {
+  let id = parseInt(req.params.id);
+  console.log(`Delete User with id ${id}`)
+  try {
+    let user = await User.destroy({ where: { id } })
+    res.sendStatus(200)
+  } catch (error) {
+    res.status(422).send(error);
+  }
+});
+
+
 app.listen(5000, () => console.log("The node.js app is listening on port 5000."));
 
 function connectToDatabase() {
@@ -38,7 +151,7 @@ function connectToDatabase() {
       console.log("Connection has been established successfully.");
 
       //Check if database was seeded already, and do it if needed
-      User.findById(1).then(user => {
+      User.find({where: {username: 'admin'}}).then(user => {
         if (!user) {
           console.log("Database is not seeded, will run seeds now...");
           const { exec } = require("child_process");
--- web-client/src/App.js
+++ web-client/src/App.js
@@ -4,20 +4,106 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "Loading...",
+      username: '',
+      password: '',
+      search: '',
+      users: []
     };
   }
 
   componentDidMount() {
-    fetch("http://localhost:5000/")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ message: data.message });
-      });
+    this.fetchUsers(this.state.search);
+  }
+
+  fetchUsers(query) {
+    if (query.length < 1) {
+      fetch("http://localhost:5000/users")
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ users: data });
+        });
+    } else {
+      fetch("http://localhost:5000/users/search", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({query})
+      })
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ users: data });
+        });
+    }
+  }
+
+  handleChangeUsername = event => {
+    this.setState({ username: event.target.value});
+  };
+
+  handleChangePassword = event => {
+    this.setState({ password: event.target.value});
+  };
+
+  handleChangeSearch = event => {
+    const val = event.target.value;
+    this.setState({ search: val});
+    this.fetchUsers(val);
+  };
+
+  handleSubmit = event => {
+    let user = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    fetch("http://localhost:5000/users", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(user), // body data type must match "Content-Type" header
+      })
+      .then(res => {
+        this.fetchUsers(this.state.search)
+      })
+  };
+
+  usersList () {
+    return this.state.users.map((user) => {
+      return (
+        <div key={user.id} >
+          {user.username}
+        </div>
+      )
+    })
   }
 
   render() {
-    return <div>{this.state.message}</div>;
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height:'100vh'}}>
+        <div style={{display: 'flex', flexDirection:'column' }}>
+          <h1> Create User </h1>
+          Username:
+          <input placeholder="Username" onChange={this.handleChangeUsername} value={this.state["username"]}/>
+          <br/>
+          Password:
+          <input placeholder="Password" type="password" onChange={this.handleChangePassword} value={this.state["password"]}/>
+          <br/>
+          <button onClick={this.handleSubmit} > Create User </button>
+          <br/>
+          <h1> Search Users </h1>
+          <input placeholder="search here" onChange={this.handleChangeSearch} value={this.state["search"]}/>
+          {this.usersList()}
+        </div>
+      </div>
+    );
   }
 }
 
